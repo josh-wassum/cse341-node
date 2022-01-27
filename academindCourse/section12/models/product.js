69471f3fc336584596624c1dd92a0d464f81de08
josh-wassum <joshuawassum@gmail.com>
2022-01-26 18:08:58 -0700

    Additional work completed

--- academindCourse/section12/models/product.js
+++ academindCourse/section12/models/product.js
@@ -10,9 +10,25 @@ class Product {
 
   save() {
     const db = getDb();
-    db.collection('products').insertOne(this)
+    return db.collection('products')
+    .insertOne(this)
     .then( result => {
       console.log(result);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+    .collection('products')
+    .find()
+    .toArray()
+    .then(products => {
+      console.log(products);
+      return products;
     })
     .catch(err => {
       console.log(err);
