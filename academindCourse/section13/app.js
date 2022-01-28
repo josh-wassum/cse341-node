7e6299cdf521764df26ec255c345ff0d80746e2f
josh-wassum <joshuawassum@gmail.com>
2022-01-27 21:58:24 -0700

    Added section 13

---
+++ academindCourse/section13/app.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user')
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findUserById('61f2ec71f34408f19d6c336f')
+    .then(user => {
+      req.user = new User(user.username, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+
+mongoose.connect('mongodb+srv://Josh:gfa2cxm-RMA!fxt*bvd@cluster0.ktcgf.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result => {
+  app.listen(3000);
+})
+.catch(err => {
+  console.log(err);
+});
