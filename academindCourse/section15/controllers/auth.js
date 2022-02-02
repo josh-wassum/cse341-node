7abdb5c8a1d3a7250850de3771ceaffdfe7f4e32
josh-wassum <joshuawassum@gmail.com>
2022-02-02 15:01:10 -0700

    Added auth to section 15

--- academindCourse/section15/controllers/auth.js
+++ academindCourse/section15/controllers/auth.js
@@ -24,14 +24,30 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('61f41920efe6558df11927db')
+  const email = req.body.email;
+  const password = req.body.password;
+    User.findOne({email: email})
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect('/');
+      if (!user) {
+        res.redirect('/login');
+      }
+      bcrypt.compare(password, user.password)
+      .then(doMatch => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+            res.redirect('/');
       });
+        }
+        res.redirect('login');
+      })
+      .catch(err => {
+        console.log(err);
+        res.redirect('/login');
+      });
+      
     })
     .catch(err => console.log(err));
 };
